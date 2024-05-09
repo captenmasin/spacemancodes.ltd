bfc054325aa92a6ace38ed9ae1ca6f477e465ab4
Mason Day <daymason@hotmail.co.uk>
2024-05-09 15:32:50 +0100

    Image sizing

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+$version = '1.0.0';
+?>
+
 <!doctype html>
 <html class="no-js h-full font-inter" lang="">
 
@@ -5,7 +9,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>SpacemanCodes LTD</title>
-    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="css/app.css?v=<?= $version ?>">
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:300,400,500,600|poppins:600,600i,700,700i" rel="stylesheet"/>
 
@@ -16,7 +20,7 @@
     <meta property="og:url" content="https://spacemancodes.ltd">
     <meta property="og:image" content="https://spacemancodes.ltd/img/og.png">
 
-    <link rel="icon" href="/favicon.ico?v=1" sizes="any">
+    <link rel="icon" href="/favicon.ico?v=<?= $version ?>" sizes="any">
     <link rel="icon" href="/icon.svg" type="image/svg+xml">
     <link rel="apple-touch-icon" href="icon.png">
 
@@ -27,7 +31,7 @@
 <body class="bg-black text-white h-full bg-cover bg-center lazy-background">
 <main class="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
     <div class="text-center">
-        <img src="icon.svg?v=1" alt="SpacemanCodes LTD" class="w-56 mx-auto mb-4">
+        <img src="icon.svg?v=<?= $version ?>" alt="SpacemanCodes LTD" class="w-28 lg:w-56 mx-auto mb-4">
         <h1 class="mt-4 text-3xl font-poppins font-semibold sm:text-5xl">
             SpacemanCodes LTD
         </h1>
@@ -35,6 +39,6 @@
 </main>
 </body>
 
-<script src="js/app.js?v=1"></script>
+<script src="js/app.js?v=<?= $version ?>"></script>
 
 </html>
